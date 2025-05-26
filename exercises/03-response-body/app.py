d6234d2573e18a13555099bddf0e9a66fa8d5117
pcaliman2 <pcaliman@gmail.com>
2025-05-26 01:29:53 +0000

    Ejercicio_03

--- exercises/03-response-body/app.py
+++ exercises/03-response-body/app.py
@@ -1,3 +1,10 @@
 import requests
 
 url = "https://assets.breatheco.de/apis/fake/sample/random-status.php"
+response = requests.get(url)
+respuesta = response.status_code
+
+if (respuesta==200):
+    print(response.text) 
+else:
+    print("Something went wrong")
