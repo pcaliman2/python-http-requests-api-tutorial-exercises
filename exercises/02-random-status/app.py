cae1faf7f0c390c8e967329e3bffb46e14d2a2c2
pcaliman2 <pcaliman@gmail.com>
2025-05-26 01:25:33 +0000

    Cambios_01

--- exercises/02-random-status/app.py
+++ exercises/02-random-status/app.py
@@ -1,3 +1,16 @@
 import requests
 
 response = requests.get("https://assets.breatheco.de/apis/fake/sample/random-status.php")
+respuesta = response.status_code
+#print(respuesta)
+
+if (respuesta== 404):
+    print("The URL you asked for is not found")
+elif (respuesta== 503):
+    print("Unavailable right now")
+elif (respuesta== 200):
+    print("Everything went perfect")
+elif (respuesta== 400):
+    print("Something is wrong with the request params")
+else:
+    print("Unknown status code")
