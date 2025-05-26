808b3d1f177c0fc33a80161c637744cb678a6b06
pcaliman2 <pcaliman@gmail.com>
2025-05-26 01:43:13 +0000

    Ejercicio_04

--- exercises/04-response-body-json/app.py
+++ exercises/04-response-body-json/app.py
@@ -2,3 +2,10 @@ import requests
 
 response = requests.get("https://assets.breatheco.de/apis/fake/sample/time.php")
 print(response.text)
+
+hora=17
+minutos=06
+segundos=23
+
+print(f'Current time: {hora} hrs {minutos} min and {segundos} sec')
+
