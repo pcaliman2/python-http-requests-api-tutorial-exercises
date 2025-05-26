d13fbd0241e45129896704852876e4db09b3cad3
pcaliman2 <pcaliman@gmail.com>
2025-05-26 18:41:14 +0000

    Ejercicio_11a

--- exercises/11-get-attachment-by-id/app.py
+++ exercises/11-get-attachment-by-id/app.py
@@ -1,7 +1,19 @@
 import requests
 
 def get_attachment_by_id(attachment_id):
-    # Your code here
-    return None
+    response = requests.get("https://assets.breatheco.de/apis/fake/sample/weird_portfolio.php")
+    respuesta = response.status_code
+
+
+    if (respuesta==200):
+        Datos = response.json()
+
+        for post in Datos["posts"]:
+                for attachment in post["attachments"]:
+                    if attachment["id"] == attachment_id:
+                        return attachment["title"]
+        return print("El Attachment no fue Encontrado") 
+    else:
+        print("Something went wrong")
 
 print(get_attachment_by_id(137))
