52aefdb906d874a8b76948391c142e4e9a7ab7ab
pcaliman2 <pcaliman@gmail.com>
2025-05-26 18:00:57 +0000

    Ejercicio_09c

--- exercises/09-list-of-blog-titles/app.py
+++ exercises/09-list-of-blog-titles/app.py
@@ -1,21 +1,24 @@
 import requests
 
 def get_titles():
+    response = requests.get("https://assets.breatheco.de/apis/fake/sample/weird_portfolio.php")
+    respuesta = response.status_code
     ListaTitulos= []
-    Datos = response.json()
-    largo = len(Datos["posts"])
-    for x in range(largo):
-        PrimerPost = Datos["posts"][x]["title"]
-        ListaTitulos.append(PrimerPost)
-    #print(ListaTitulos)
+
+    if (respuesta==200):
+
+        Datos = response.json()
+        largo = len(Datos["posts"])
+        for x in range(largo):
+            PrimerPost = Datos["posts"][x]["title"]
+            ListaTitulos.append(PrimerPost)
+    else:
+        print("Something went wrong")
     return ListaTitulos
 
 
+print(get_titles())
 
-response = requests.get("https://assets.breatheco.de/apis/fake/sample/weird_portfolio.php")
-respuesta = response.status_code
 
-if (respuesta==200):
-    print(get_titles())
-else:
-    print("Something went wrong")
+
+
