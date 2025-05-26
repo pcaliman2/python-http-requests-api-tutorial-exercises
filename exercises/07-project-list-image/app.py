84afc49e5c1ce511759018c790f8be2e0229ca88
pcaliman2 <pcaliman@gmail.com>
2025-05-26 16:52:33 +0000

    Ejercicio_07c

--- exercises/07-project-list-image/app.py
+++ exercises/07-project-list-image/app.py
@@ -3,19 +3,16 @@ import json
 
 response = requests.get("https://assets.breatheco.de/apis/fake/sample/project_list.php")
 #Datos = json.loads(response.text)
-Datos = response.json()
-NumeroElentos = len(Datos)
-
-last_project = Datos[-1]
-#last_image_url = last_project["images"][-1]
-
-ImagenesCurso = last_project["images"]
-
 respuesta = response.status_code
-last_image_url = last_project["images"][-1]
+
 if (respuesta==200):
-    #print(ImagenesCurso) 
-    #print(last_project) 
+    Datos = response.json()
+    NumeroElentos = len(Datos)
+    UltimoProyecto = Datos[-1]
+    ImagenesCurso = UltimoProyecto["images"]
+
+    elementos = len(UltimoProyecto)
+    last_image_url = last_project["images"][-1]
     print(last_image_url)
 else:
     print("Something went wrong")
