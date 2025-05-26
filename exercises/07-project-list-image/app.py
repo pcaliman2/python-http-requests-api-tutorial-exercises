083a8841d4b371bb97b82eac4b223fbb23f3b821
pcaliman2 <pcaliman@gmail.com>
2025-05-26 17:04:48 +0000

    Pregunta_7e

--- exercises/07-project-list-image/app.py
+++ exercises/07-project-list-image/app.py
@@ -9,7 +9,7 @@ if (respuesta==200):
     Datos = response.json()
     UltimoProyecto = Datos[(len(Datos))-1]
     ImagenesCurso = UltimoProyecto["images"]
-    url_UltimaImagen = last_project["images"][-1]
+    url_UltimaImagen = ImagenesCurso[-1]
     print(url_UltimaImagen)
 else:
     print("Something went wrong")
