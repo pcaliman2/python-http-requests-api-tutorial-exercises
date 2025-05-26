80226a105208e415c38bdf713d5983e1473d8120
pcaliman2 <pcaliman@gmail.com>
2025-05-26 18:25:50 +0000

    Ejercicio_10b

--- exercises/10-get-post-tags/app.py
+++ exercises/10-get-post-tags/app.py
@@ -1,8 +1,20 @@
 import requests
+import json
 
 def get_post_tags(post_id):
-    # Your code here
-    return None
+    response = requests.get("https://assets.breatheco.de/apis/fake/sample/weird_portfolio.php")
+    respuesta = response.status_code
 
+
+    if (respuesta==200):
+        Datos = response.json()
+
+        for post in Datos["posts"]:
+            if post["id"] == post_id:
+                return post["tags"]
+        return print("El Post no fue Encontrado") 
+    else:
+        print("Something went wrong")
+    
 
 print(get_post_tags(146))
