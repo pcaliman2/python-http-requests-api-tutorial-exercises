cec2c3abefafa646bd4cb7b5a7d0cda79e0bf189
pcaliman2 <pcaliman@gmail.com>
2025-05-26 17:11:26 +0000

    Ejercicio_08a

--- exercises/08-blog-post-author/app.py
+++ exercises/08-blog-post-author/app.py
@@ -1,3 +1,17 @@
 import requests
+import json
 
-# Your code here
+response = requests.get("https://assets.breatheco.de/apis/fake/sample/weird_portfolio.php")
+#Datos = json.loads(response.text)
+respuesta = response.status_code
+
+if (respuesta==200):
+    Datos = response.json()
+    UltimoProyecto = Datos[(len(Datos))-1]
+    ImagenesCurso = UltimoProyecto["images"]
+    url_UltimaImagen = ImagenesCurso[-1]
+    print(url_UltimaImagen)
+else:
+    print("Something went wrong")
+
+
